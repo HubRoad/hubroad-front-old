ad9ed30193ebc313fecc012f787b6bce8e04c597
Florian Wahl <florian.wahl@hotmail.fr>
2016-09-29 21:58:53 -0500

    fix: typedoc error

---
+++ src/app/dashboard/applications-lifecycle-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'hr-applications-lifecycle-details',
+    templateUrl: 'applications-lifecycle-details.component.html'
+})
+export class ApplicationsLifecycleDetailsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
