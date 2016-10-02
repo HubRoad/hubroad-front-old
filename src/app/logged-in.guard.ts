d1f3ac0d265c782fc08482120a819088ae78f84b
Florian Wahl <florian.wahl@hotmail.fr>
2016-10-01 22:09:05 -0500

    redirect to home if not logged in

--- src/app/logged-in.guard.ts
+++ src/app/logged-in.guard.ts
@@ -1,16 +1,18 @@
 import {Injectable} from "@angular/core";
-import {CanActivate, CanActivateChild} from "@angular/router";
+import {CanActivate, CanActivateChild, Router} from "@angular/router";
 import {AuthentificationService} from "./authentification.service";
 
 @Injectable()
 export class LoggedInGuard implements CanActivate, CanActivateChild {
-  constructor(private user: AuthentificationService) {}
+  constructor(private user: AuthentificationService, private router: Router) {}
 
-  canActivate(): boolean {
-    return this.user.isLoggedIn();
+  canActivate() {
+    if(this.user.isLoggedIn())
+      return true;
+    this.router.navigate(['']);
   }
 
-  canActivateChild(): boolean {
+  canActivateChild() {
     return this.canActivate();
   }
 }
